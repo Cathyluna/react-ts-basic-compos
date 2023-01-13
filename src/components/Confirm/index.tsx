1290cd58587f4272699983a28e8ae965f044bab4
Cathy Zhang <69488671+Cathyluna@users.noreply.github.com>
2023-01-13 17:01:00 +0800

    Feature/1.0 (#1)
    * feat: Add Confirm Component

---
+++ src/components/Confirm/index.tsx
@@ -0,0 +1,85 @@
+/**
+ * 禁止背景滑动的确认弹窗
+ */
+import ReactDOM from "react-dom";
+import style from './index.module.css'
+
+interface ButtonListParams {
+  text: string;
+  handleClick: () => void;
+}
+
+export interface ConfirmModalProps {
+  header?: string;
+  description?: string;
+  buttonList?: ButtonListParams[];
+  handleClose: () => void;
+}
+
+const ConfirmModal = ({
+  header = "",
+  description = "",
+  buttonList = [],
+  handleClose = () => {},
+}: ConfirmModalProps) => (
+  <article className={style.mask}>
+    <article className={style.container}>
+      {!!header && <header className={style.header}>{header}</header>}
+      {!!description && <section className={style.description}>{description}</section>}
+      {!!buttonList?.length && (
+        <footer className={style.footer}>
+          {buttonList.map((item) => (
+            <button
+              className={style.confirmBtn}
+              onClick={() => {
+                if (item?.handleClick) {
+                  item.handleClick();
+                }
+                handleClose();
+              }}
+            >
+              {item?.text}
+            </button>
+          ))}
+        </footer>
+      )}
+    </article>
+  </article>
+);
+
+const container = document.createElement("div");
+
+const Confirm = {
+  show: (params: ConfirmModalProps) => {
+    // 禁止背景滚动
+    const stopScroll = () => {
+      const topVal = document.body.scrollTop || document.documentElement.scrollTop;
+      console.log('禁止滚动', topVal)
+      document.body.style.position = "fixed";
+      document.body.style.top = `${-topVal}px`
+    }
+
+    // 恢复背景滚动
+    const recoverScroll = () => {
+      const topVal = -parseInt(document.body.style.top)
+      console.log('恢复滚动', topVal)
+      document.body.style.position = "static";
+      document.body.style.top = '0';
+      window.scrollTo(0, topVal)
+    }
+    
+    const handleClose = () => {
+      document.body.removeChild(container);
+      recoverScroll()
+    }
+
+    ReactDOM.render(<ConfirmModal {...params} handleClose={handleClose} />, container);
+
+    document.body.appendChild(container);
+
+    stopScroll();
+
+  },
+};
+
+export default Confirm;
---
+++ src/components/Confirm/readme.md
@@ -0,0 +1,45 @@
+## Confirm 弹窗组件
+
+### 组件功能
+
+1. 阻止背景滚动；
+2. Confirm组件可传按钮列表，自定义按钮点击事件；
+3. 调用简单方便；
+
+   ```
+    // 举个栗子
+    Confirm.show({
+        header: '这是标题',
+        description: '内容里面有些注释文字展示，展示什么什么什么',
+        buttonList: [
+            {
+                text: '确认',
+                handleClick: () => console.log('点击确认'),
+            },
+            {
+                text: '取消',
+                handleClick: () => console.log('点击取消'),
+            },
+            {
+                text: '我不知道',
+                handleClick: () => console.log('我不知道'),
+            },
+        ],
+    } as ConfirmModalProps)
+   ```
+
+### 需要优化
+
+- [ ] 样式优化
+
+### 技术实现Point
+
+- 阻止背景滚动
+  
+  - 修改body元素样式为 `position: fixed` 
+  - 再通过获取当前文档 html `document.documentElement.scroolTop` 或者 body `document.body.scrollTop` 垂直滚动的像素数来滚动到指定位置；
+
+
+### 知识点扩展
+
+1. [ ] 滚动穿透问题的常见解决方法（待补充）
